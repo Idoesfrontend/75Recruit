be4a97f3cfb0134b6eaff8d28e347c989297b6b4
Idoesfrontend <121342182+Idoesfrontend@users.noreply.github.com>
2023-11-17 10:57:11 +0800

    asdkjaslkda

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -26,8 +26,8 @@ const MobileMenu = () => {
           </p>
           <p>
             <a href="mailto:Apply75Recruit@outlook.com">Ashley@75Recruit.com</a> <br />
-            <a href="tel:+86-151-386-93451">live:.cid.3ecf09b6e26552ac</a> <br/>
-            <a href="tel:86-155-155-12405">+1-716-427-2129 (WhatsApp)</a>
+            <a href="tel:live:.cid.3ecf09b6e26552ac">live:.cid.3ecf09b6e26552ac</a> <br/>
+            <a target="_blank" href="http://wa.me/17164272129">+1-716-427-2129 (WhatsApp)</a>
           </p>
           {/*<div className="side-menu__social">*/}
           {/*  <a href="#">*/}
